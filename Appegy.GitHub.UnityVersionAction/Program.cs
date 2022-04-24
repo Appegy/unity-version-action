c668a0dedd71c21377fc515687b566af48aa36f3
Ivan Murashka <imurashka.me@gmail.com>
2022-04-24 14:09:06 +0200

    implement action logic

--- Appegy.GitHub.UnityVersionAction/Program.cs
+++ Appegy.GitHub.UnityVersionAction/Program.cs
@@ -1,3 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
+using Appegy.GitHub.UnityVersionAction;
+using CommandLine;
 
-Console.WriteLine("Hello, World!");
+var input = parseArguments(args);
+if (input == null)
+{
+    Environment.Exit(1);
+    return;
+}
+
+var version = findUnityVersion(input.ProjectPath);
+if (version == null)
+{
+    Environment.Exit(1);
+    return;
+}
+
+Console.WriteLine($"::set-output name=version::{version.Version}");
+Console.WriteLine($"::set-output name=changeset::{version.Changeset}");
+if (!string.IsNullOrEmpty(input.VersionEnv))
+{
+    Environment.SetEnvironmentVariable(input.VersionEnv, version.Version);
+}
+if (!string.IsNullOrEmpty(input.ChangesetEnv))
+{
+    Environment.SetEnvironmentVariable(input.ChangesetEnv, version.Changeset);
+}
+
+static ActionInputs? parseArguments(IEnumerable<string> args)
+{
+    ActionInputs? inputs = null;
+    Parser.Default.ParseArguments(() => new ActionInputs(), args)
+        .WithNotParsed(
+            errors =>
+            {
+                Console.Error.WriteLine(string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
+                inputs = null;
+            })
+        .WithParsed(result =>
+        {
+            Console.WriteLine($"Project path: {result.ProjectPath}");
+            var versionEnv = string.IsNullOrEmpty(result.VersionEnv) ? "Not requested" : result.VersionEnv;
+            var changesetEnv = string.IsNullOrEmpty(result.ChangesetEnv) ? "Not requested" : result.ChangesetEnv;
+            Console.WriteLine($"Version Env: {versionEnv}");
+            Console.WriteLine($"Changeset Env: {changesetEnv}");
+            inputs = result;
+        });
+
+    return inputs;
+}
+
+static UnityVersion? findUnityVersion(string projectPath)
+{
+    var path = Path.Combine(projectPath, "ProjectSettings", "ProjectVersion.txt");
+    if (!File.Exists(path))
+    {
+        Console.Error.WriteLine($"Could not find {path}");
+        return null;
+    }
+    var file = File.ReadAllText(path);
+    var match = Regex.Match(file, @"m_EditorVersionWithRevision: (.+) \((.+)\)");
+    if (match.Success)
+    {
+        return new UnityVersion(match.Groups[1].Value, match.Groups[2].Value);
+    }
+    match = Regex.Match(file, @"m_EditorVersion: (.+) \((.+)\)");
+    if (match.Success)
+    {
+        return new UnityVersion(match.Groups[1].Value, string.Empty);
+    }
+    Console.Error.WriteLine($"Could not find version in {path}");
+    return null;
+}
---
+++ Appegy.GitHub.UnityVersionAction/UnityVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace Appegy.GitHub.UnityVersionAction;
+
+public class UnityVersion
+{
+    public string Version { get; }
+    public string Changeset { get; }
+
+    public UnityVersion(string version, string changeset)
+    {
+        Version = version;
+        Changeset = changeset;
+    }
+}
